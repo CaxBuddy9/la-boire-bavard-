4fc3de74c792ea11e526f022b64f7576cfa8ee38
La Boire Bavard <laboirebavard@gmail.com>
2026-04-15 12:07:54 +0700

    SEO : sitemap corrigé, robots.txt, JSON-LD BedAndBreakfast schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,9 +57,55 @@ export const viewport: Viewport = {
   themeColor: '#0a0f0a',
 }
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'BedAndBreakfast',
+  name: 'La Boire Bavard',
+  description: "Maison d'hôtes de charme entre Angers et Saumur. Piscine chauffée, spa, petit-déjeuner gourmand. Note 9.9/10.",
+  url: 'https://la-boire-bavard.vercel.app',
+  telephone: '+33675786335',
+  email: 'laboirebavard@gmail.com',
+  priceRange: '88€',
+  currenciesAccepted: 'EUR',
+  address: {
+    '@type': 'PostalAddress',
+    streetAddress: '4 chemin de la Boire Bavard, Lieu-dit La Hutte',
+    addressLocality: 'Blaison-Saint-Sulpice',
+    postalCode: '49320',
+    addressCountry: 'FR',
+  },
+  geo: {
+    '@type': 'GeoCoordinates',
+    latitude: 47.368,
+    longitude: -0.511,
+  },
+  aggregateRating: {
+    '@type': 'AggregateRating',
+    ratingValue: '9.9',
+    bestRating: '10',
+    ratingCount: '50',
+  },
+  amenityFeature: [
+    { '@type': 'LocationFeatureSpecification', name: 'Piscine chauffée',      value: true },
+    { '@type': 'LocationFeatureSpecification', name: 'Spa / Jacuzzi',         value: true },
+    { '@type': 'LocationFeatureSpecification', name: 'Petit-déjeuner inclus', value: true },
+    { '@type': 'LocationFeatureSpecification', name: 'WiFi gratuit',          value: true },
+    { '@type': 'LocationFeatureSpecification', name: 'Table d\'hôtes',        value: true },
+    { '@type': 'LocationFeatureSpecification', name: 'Jardin',                value: true },
+  ],
+  hasMap: 'https://maps.google.com/?q=47.368,-0.511',
+  image: 'https://la-boire-bavard.vercel.app/photos/exterieur/maison-facade-printemps.jpg',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className={`${playfair.variable} ${raleway.variable} ${garamond.variable}`}>
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body>
         <LangProvider>
           {children}
---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin', '/api/'],
+    },
+    sitemap: 'https://la-boire-bavard.vercel.app/sitemap.xml',
+  }
+}
